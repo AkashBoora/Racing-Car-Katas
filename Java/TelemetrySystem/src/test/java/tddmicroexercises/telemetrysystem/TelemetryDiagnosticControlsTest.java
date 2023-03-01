4a8a5632b8d08952f541d1bff9d7b173e3b5854a
Akash Boora <akash.boora@zemosolabs.com>
2023-03-01 11:46:55 +0530

    made review changes

--- Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -7,7 +7,8 @@ public class TelemetryDiagnosticControlsTest
     
 	@Test
     public void checkTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
-		TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls();
+		TelemetryClient telemetryClient = new TelemetryClient();
+		TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
 		telemetryDiagnosticControls.checkTransmission();
     }
 }
